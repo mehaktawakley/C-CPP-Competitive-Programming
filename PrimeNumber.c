72989e743701bc466ff69cfecfdc5d4efa8e6035
Mehak Tawakley <32432186+mehaktawakley@users.noreply.github.com>
2019-01-24 23:43:24 +0530

    Init commit

--- PrimeNumber.c
+++ PrimeNumber.c
@@ -1,15 +1,12 @@
 /*You are given an integer N. You need to print the series of all prime numbers till N.
 
 Input Format
-
 The first and only line of the input contains a single integer N denoting the number till where you need to find the series of prime number.
 
 Output Format
-
 Print the desired output in single line separated by spaces.
 
 Constraints
-
 1<=N<=1000
 
 SAMPLE INPUT 
