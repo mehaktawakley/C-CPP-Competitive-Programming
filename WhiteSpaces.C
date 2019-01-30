619c5a0bd81619d33ba39cfce5f7baee72e79d76
Mehak Tawakley <32432186+mehaktawakley@users.noreply.github.com>
2019-01-30 22:49:04 +0530

    init commit

--- WhiteSpaces.C
+++ WhiteSpaces.C
@@ -2,16 +2,14 @@
 Given A String S Find The Number Of White Spaces In The Given String S.
 
 Input Format
-
 Take Input String S
 
 Output Format
-
 Number Of White Spaces In S
 
 Constraints
-
 0 < S < 100001
+
 SAMPLE INPUT 
 Hello World
 SAMPLE OUTPUT 
