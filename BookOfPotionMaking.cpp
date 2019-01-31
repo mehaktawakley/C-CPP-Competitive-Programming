b8f8acd6069965c511ed0da44e03ad5c8b74095c
Mehak Tawakley <32432186+mehaktawakley@users.noreply.github.com>
2019-01-31 22:18:51 +0530

    init commit

--- BookOfPotionMaking.cpp
+++ BookOfPotionMaking.cpp
@@ -4,8 +4,9 @@ Harry wants to find Voldemort's potion making book but he is confused about how 
 The book has a special ISBN(International Standard Book Number) which is  unique numeric book identifier only for Voldemort's book printed on it. The ISBN is based upon a 10-digit code. The ISBN is valid if:
 1xdigit1 + 2xdigit2 + 3xdigit3 + 4xdigit4 + 5xdigit5 + 6xdigit6 + 7xdigit7 + 8xdigit8 + 9xdigit9 + 10xdigit10 is divisible by 11.
 Help Harry to find the book!
+
 Input:
- Input the ISBN code as a 10-digit integer.
+Input the ISBN code as a 10-digit integer.
 
 Output:
 If the ISBN is not a 10-digit integer, output the message “Illegal ISBN” and terminate the program.
