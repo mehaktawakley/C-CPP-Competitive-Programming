6bea6273502e45e8582e3a9a92e7dc007e6efc41
Mehak Tawakley <32432186+mehaktawakley@users.noreply.github.com>
2019-03-25 23:40:52 +0530

    init commit

--- ChanduAndHisPlayingArea.C
+++ ChanduAndHisPlayingArea.C
@@ -1,14 +1,10 @@
 /*
-Little chandu is very fond of playing games. Recently, He found a few straws each of length 
-1
- inches in the store room. He took all of them and decided to mark a rectangular area on the floor with straws and warn rest of the family members to not to enter that area so that he can play in peace. He wants to maximize that area. But, unable to do so, He seeks for your help. You being the elder brother of chandu, write a program for him to find maximum area that he can cover in inches using N straws.
+Little chandu is very fond of playing games. Recently, He found a few straws each of length 1 inches in the store room. He took all of them and decided to mark a rectangular area on the floor with straws and warn rest of the family members to not to enter that area so that he can play in peace. He wants to maximize that area. But, unable to do so, He seeks for your help. You being the elder brother of chandu, write a program for him to find maximum area that he can cover in inches using N straws.
 
 Input:
-
 First line of input contains an integer t. then, t lines follow each containing a single integer N - No of straws.
 
 Output:
-
 Print the area of largest rectangle that can be formed using the given sticks.
 
 SAMPLE INPUT 
