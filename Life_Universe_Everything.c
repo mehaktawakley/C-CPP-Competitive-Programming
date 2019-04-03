60d5623ab06e4565a51adc1afa5c514a4fcb7f78
Mehak Tawakley <32432186+mehaktawakley@users.noreply.github.com>
2019-04-03 23:27:17 +0530

    Update Life_Universe_Everything.c

--- Life_Universe_Everything.c
+++ Life_Universe_Everything.c
@@ -1,4 +1,7 @@
-/*Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.
+/*
+Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. 
+More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. 
+All numbers at input are integers of one or two digits.
 
 SAMPLE INPUT 
 1
@@ -9,19 +12,17 @@ SAMPLE INPUT
 SAMPLE OUTPUT 
 1
 2
-88*/
+88
+*/
 
 
 #include<stdio.h>
 
-int main()
-{
+int main(){
     int x;
-    while(1)
-    {
+    while(1){
         scanf("%d",&x);
-        if(x==42)
-        {
+        if(x==42){
             break;
         }
         else
